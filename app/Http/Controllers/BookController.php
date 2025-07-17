04b463c1a287d9c554c932bdd81e8aada8597af4
AlicaLynn17 <alicalynntabada@gmail.com>
2025-07-17 21:00:38 +0800

    welcome

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,7 @@ class BookController extends Controller
     public function index()
     {
         $books = Book::all();
-        return view('books.index', compact('books'));
+        return view('welcome', compact('books'));
     }
 
     public function search(Request $request)
